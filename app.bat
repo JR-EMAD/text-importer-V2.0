cdb2d2e1609f03be903e25a45dbfcf391a10aa41
JR-EMAD <152868789+JR-EMAD@users.noreply.github.com>
2024-03-14 22:02:26 +0330

    V2.0
    is best
    trust me!

---
+++ app.bat
@@ -0,0 +1,45 @@
+@echo off
+echo ____________________________________________________
+echo ____             auto text importer             ____
+echo ____  *if you need more line add it in source   ____
+echo ____                BY JR_EMAD                  ____
+echo ____________________________________________________
+echo app successfuly run! import text
+set /P l1="line 1:"
+set /P l2="line 2:"
+set /P l3="line 3:"
+set /P l4="line 4:"
+set /P l5="line 5:"
+set /P l6="line 6:"
+set /P l7="line 7:"
+set /P l8="line 8:"
+set /P l9="line 9:"
+set /P l10="line10:"
+echo importing started!
+timeout /t 1 /nobreak > NUL
+echo 1 %
+timeout /t 1 /nobreak > NUL
+echo 6 %
+timeout /t 1 /nobreak > NUL
+echo 39 %
+timeout /t 1 /nobreak > NUL
+echo 64 %
+timeout /t 1 /nobreak > NUL
+echo 81 %
+timeout /t 1 /nobreak > NUL
+echo 100 %
+timeout /t 1 /nobreak > NUL
+echo your file is ready enjoy!
+echo @echo off >> ati.bat
+echo echo %l1% >> ati.bat
+echo echo %l2% >> ati.bat
+echo echo %l3% >> ati.bat
+echo echo %l4% >> ati.bat
+echo echo %l5% >> ati.bat
+echo echo %l6% >> ati.bat
+echo echo %l7% >> ati.bat
+echo echo %l8% >> ati.bat
+echo echo %l9% >> ati.bat
+echo echo %l10% >> ati.bat
+echo pause >> ati.bat
+pause
